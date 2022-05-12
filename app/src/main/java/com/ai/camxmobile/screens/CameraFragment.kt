34c8bd1389d0cb4675caca3204e85c7d92dc7814
CephasPeter <52112132+CephasPeter@users.noreply.github.com>
2022-05-12 19:05:09 +0100

    Setting Up Project

--- app/src/main/java/com/ai/camxmobile/screens/CameraFragment.kt
+++ app/src/main/java/com/ai/camxmobile/screens/CameraFragment.kt
@@ -1,60 +1,280 @@
-package com.ai.camxmobile.screens
+package com.ai.camx.screens
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.usage.UsageStatsManager
+import android.content.ContentValues
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.*
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.ScaleGestureDetector
 import android.view.View
 import android.view.ViewGroup
-import com.ai.camxmobile.R
+import android.widget.Toast
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.video.Recorder
+import androidx.camera.video.Recording
+import androidx.camera.video.VideoCapture
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.ai.camx.R
+import com.ai.camx.databinding.FragmentCameraBinding
+import com.ai.camx.viewmodels.CameraViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import java.nio.ByteBuffer
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CameraFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+typealias LumaListener = (luma: Double) -> Unit
+
 class CameraFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentCameraBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private var imageCapture: ImageCapture? = null
+
+    private var videoCapture: VideoCapture<Recorder>? = null
+    private var recording: Recording? = null
+
+    private lateinit var cameraExecutor: ExecutorService
+
+    private val camViewModel: CameraViewModel by activityViewModels()
+
+    private val mainScope = CoroutineScope(Dispatchers.Main + Job())
+    private val ioScope = CoroutineScope(Dispatchers.IO + Job())
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentCameraBinding.inflate(inflater,container,false)
+
+        //val usageStatsManager = requireActivity().getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(requireActivity(), REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+        binding.shutterForeground.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                val vibratorManager = requireActivity().getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
+                val vibrator = vibratorManager.defaultVibrator
+
+                vibrator.vibrate(VibrationEffect.createOneShot(100, VibrationEffect.DEFAULT_AMPLITUDE))
+            }else{
+                val vibrator = context?.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+                if (Build.VERSION.SDK_INT >= 26) {
+                    vibrator.vibrate(VibrationEffect.createOneShot(100, VibrationEffect.DEFAULT_AMPLITUDE))
+                } else {
+                    vibrator.vibrate(100)
+                }
+            }
+
+            binding.shutterForeground.visibility = View.GONE
+            binding.shutterEffectView.visibility = View.VISIBLE
+            takePhoto()
+        }
+
+        binding.flipCamera.setOnClickListener {
+            if(camViewModel.lensFacing.value == CameraSelector.DEFAULT_FRONT_CAMERA){
+                camViewModel.lensFacing.value = CameraSelector.DEFAULT_BACK_CAMERA
+            }else{
+                camViewModel.lensFacing.value = CameraSelector.DEFAULT_FRONT_CAMERA
+            }
+        }
+
+        return binding.root
+    }
+
+    private fun takePhoto() {
+        val imageCapture = imageCapture ?: return
+
+        // Create time stamped name and MediaStore entry.
+        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.getDefault()).format(System.currentTimeMillis())
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            if(Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CamX")
+            }
+        }
+
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(requireActivity().contentResolver, MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues).build()
+
+        imageCapture.takePicture(outputOptions, ContextCompat.getMainExecutor(requireActivity()), object : ImageCapture.OnImageSavedCallback {
+            override fun onError(exc: ImageCaptureException) {
+                Toast.makeText(requireContext(),"An Error Occurred",Toast.LENGTH_SHORT).show()
+                binding.shutterForeground.visibility = View.VISIBLE
+                binding.shutterEffectView.visibility = View.GONE
+            }
+
+            override fun onImageSaved(output: ImageCapture.OutputFileResults){
+                binding.shutterEffectView.visibility = View.GONE
+                binding.shutterForeground.visibility = View.VISIBLE
+            }
+        })
+    }
+
+    private var lensFacing = CameraSelector.DEFAULT_BACK_CAMERA
+    @SuppressLint("ClickableViewAccessibility")
+    private fun startCamera() {
+        if(camViewModel.lensFacing.value !=null){
+            lensFacing = camViewModel.lensFacing.value!!
+        }else{
+            camViewModel.lensFacing.value = lensFacing
+        }
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireActivity())
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+
+            imageCapture = ImageCapture.Builder().build()
+
+            val imageAnalyzer = ImageAnalysis.Builder()
+                .build()
+                .also {
+                    it.setAnalyzer(cameraExecutor, LuminosityAnalyzer { luma ->
+                        mainScope.launch {
+                            binding.light.text = luma.toInt().toString()
+                        }
+                        Log.d(TAG, "Average luminosity: $luma")
+                    })
+                }
+
+            var camera: Camera? = null
+            var cameraInfo: CameraInfo? = null
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                camera = cameraProvider.bindToLifecycle(this, lensFacing, preview, imageCapture, imageAnalyzer)
+                cameraInfo = camera.cameraInfo
+            } catch(exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+            camViewModel.lensFacing.observe(requireActivity()) {
+                try {
+                    cameraProvider.unbindAll()
+                    // Bind use cases to camera
+                    camera = cameraProvider.bindToLifecycle(this, it!!, preview, imageCapture, imageAnalyzer)
+                    cameraInfo = camera!!.cameraInfo
+                } catch(exc: Exception) {
+                    Log.e(TAG, "Use case binding failed", exc)
+                }
+            }
+
+            camViewModel.flashEnabled.observe(requireActivity()){
+                if(camera!=null && it !=null){
+                    val cameraControl = camera!!.cameraControl
+                    cameraControl.enableTorch(it)
+                    val flashState = cameraInfo!!.torchState
+                    if (flashState.value == 1){
+                        binding.flash.setImageDrawable(ContextCompat.getDrawable(requireActivity(),R.drawable.ic_round_flash_on_24))
+                    }else{
+                        binding.flash.setImageDrawable(ContextCompat.getDrawable(requireActivity(),R.drawable.ic_round_flash_off_24))
+                    }
+                }
+            }
+
+            val listener = object : ScaleGestureDetector.SimpleOnScaleGestureListener() {
+                override fun onScale(detector: ScaleGestureDetector): Boolean {
+                    if(camera!=null){
+                        val cameraControl = camera!!.cameraControl
+                        val scale = cameraInfo!!.zoomState.value!!.zoomRatio * detector.scaleFactor
+                        cameraControl.setZoomRatio(scale)
+                    }
+                    return true
+                }
+            }
+
+            val scaleGestureDetector = ScaleGestureDetector(requireActivity(), listener)
+
+            binding.flash.setOnClickListener {
+                if(camera!=null){
+                    val cameraControl = camera!!.cameraControl
+                    camViewModel.flashEnabled.value = camViewModel.flashEnabled.value != true
+                }
+            }
+
+            binding.viewFinder.setOnTouchListener { _, event ->
+                scaleGestureDetector.onTouchEvent(event)
+                return@setOnTouchListener true
+            }
+        }, ContextCompat.getMainExecutor(requireActivity()))
+    }
+
+    private fun requestPermission(){
+
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(requireActivity().baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(requireContext(), "Permissions not granted by the user.", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_camera, container, false)
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CameraFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CameraFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        private const val TAG = "CamX AI"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS =
+            mutableListOf (
+                Manifest.permission.CAMERA,
+                Manifest.permission.RECORD_AUDIO
+            ).apply {
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                    add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
                 }
-            }
+            }.toTypedArray()
+    }
+
+    private class LuminosityAnalyzer(private val listener: LumaListener) : ImageAnalysis.Analyzer {
+        private fun ByteBuffer.toByteArray(): ByteArray {
+            rewind()    // Rewind the buffer to zero
+            val data = ByteArray(remaining())
+            get(data)   // Copy the buffer into a byte array
+            return data // Return the byte array
+        }
+
+        override fun analyze(image: ImageProxy) {
+            val buffer = image.planes[0].buffer
+            val data = buffer.toByteArray()
+            val pixels = data.map { it.toInt() and 0xFF }
+            val luma = pixels.average()
+
+            listener(luma)
+
+            image.close()
+        }
     }
 }
