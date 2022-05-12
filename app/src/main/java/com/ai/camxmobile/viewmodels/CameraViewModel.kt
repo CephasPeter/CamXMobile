00a6c488d31594a1324b8202574d023d855c0814
CephasPeter <52112132+CephasPeter@users.noreply.github.com>
2022-05-12 19:04:04 +0100

    Setting Up Project

--- app/src/main/java/com/ai/camxmobile/viewmodels/CameraViewModel.kt
+++ app/src/main/java/com/ai/camxmobile/viewmodels/CameraViewModel.kt
@@ -1,4 +1,10 @@
 package com.ai.camxmobile.viewmodels
 
-class CameraViewModel {
+import androidx.camera.core.CameraSelector
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CameraViewModel : ViewModel(){
+    var lensFacing = MutableLiveData<CameraSelector?>()
+    var flashEnabled = MutableLiveData<Boolean?>()
 }
