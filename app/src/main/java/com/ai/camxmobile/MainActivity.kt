0fb291758e691337523eacef029004d0a961f4ef
CephasPeter <52112132+CephasPeter@users.noreply.github.com>
2022-05-12 19:17:39 +0100

    Added SplashScreen

--- app/src/main/java/com/ai/camxmobile/MainActivity.kt
+++ app/src/main/java/com/ai/camxmobile/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.ai.camxmobile
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.ai.camxmobile.databinding.ActivityMainBinding
+import com.google.android.material.color.MaterialColors
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        installSplashScreen()
+
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        setUpUI()
+    }
+
+    private fun setUpUI(){
+        /*when (resources?.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK)) {
+            Configuration.UI_MODE_NIGHT_YES -> {
+                val color = MaterialColors.getColor(this, R.attr.colorSecondaryContainer, Color.TRANSPARENT)
+                window.navigationBarColor = getColorWithAlpha(color,0.36f)
+            }
+            Configuration.UI_MODE_NIGHT_NO -> {
+                val color = MaterialColors.getColor(this, R.attr.colorSecondaryContainer, Color.TRANSPARENT)
+                window.navigationBarColor = getColorWithAlpha(color,0.54f)
+            }
+            Configuration.UI_MODE_NIGHT_UNDEFINED -> {
+                val color = MaterialColors.getColor(this, R.attr.colorSecondaryContainer, Color.TRANSPARENT)
+                window.navigationBarColor = getColorWithAlpha(color,0.54f)
+            }
+        }*/
+        val color = MaterialColors.getColor(this, R.attr.backgroundColor, Color.TRANSPARENT)
+        window.navigationBarColor = color
     }
 }
