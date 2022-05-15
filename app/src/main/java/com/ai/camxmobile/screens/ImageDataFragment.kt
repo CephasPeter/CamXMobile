9d04da75ec7fb31af12c4d6bdb74f7188ee0a0bc
CephasPeter <52112132+CephasPeter@users.noreply.github.com>
2022-05-15 21:09:35 +0100

    Working Screen to display image data

--- app/src/main/java/com/ai/camxmobile/screens/ImageDataFragment.kt
+++ app/src/main/java/com/ai/camxmobile/screens/ImageDataFragment.kt
@@ -21,7 +21,13 @@ class ImageDataFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentImageDataBinding.inflate(inflater,container,false)
+
+
         return binding.root
+    }
+
+    private fun setUpUI(){
+
     }
 
     private fun runLabelDetection(bitmap: Bitmap) {
