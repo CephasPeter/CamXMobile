e029848092c6d5d4fec49dbba98e6974442ef252
CephasPeter <52112132+CephasPeter@users.noreply.github.com>
2022-05-15 21:00:17 +0100

    Added Screen to display image data

--- app/src/main/java/com/ai/camxmobile/screens/ImageDataFragment.kt
+++ app/src/main/java/com/ai/camxmobile/screens/ImageDataFragment.kt
@@ -5,12 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.ai.camxmobile.R
 import com.ai.camxmobile.databinding.FragmentCameraBinding
 import com.ai.camxmobile.databinding.FragmentImageDataBinding
+import com.ai.camxmobile.viewmodels.CameraViewModel
 
 class ImageDataFragment : Fragment() {
     private lateinit var binding: FragmentImageDataBinding
+    private val camViewModel: CameraViewModel by activityViewModels()
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentImageDataBinding.inflate(inflater,container,false)
         return binding.root
