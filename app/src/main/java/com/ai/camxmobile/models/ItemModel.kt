44483446f341bd295655027ffaee6feea7e3261e
CephasPeter <52112132+CephasPeter@users.noreply.github.com>
2022-05-16 01:52:21 +0100

    Creating Room Database:    Added Type Converter for Image Label Class

--- app/src/main/java/com/ai/camxmobile/models/ItemModel.kt
+++ app/src/main/java/com/ai/camxmobile/models/ItemModel.kt
@@ -1,12 +1,20 @@
 package com.ai.camxmobile.models
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.TypeConverters
+import com.ai.camxmobile.database.typeconverters.ImageLabelConverter
 import com.google.mlkit.vision.label.ImageLabel
 
 @Entity
-data class ItemModel(var id: String){
+data class ItemModel(
+    @PrimaryKey
+    var id: String
+){
     var uri:String? =null
     var name:String? = null
     var createdDate: Long? = null
+
+    @TypeConverters(ImageLabelConverter::class)
     var imageLabel: ImageLabel? = null
 }
